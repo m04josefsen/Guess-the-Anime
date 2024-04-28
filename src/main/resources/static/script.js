76752075cab4b0bb74e0746d48c3bb9a510a5a5a
Marco Josefsen <majos6959@oslomet.no>
2024-04-28 17:23:13 +0200

    Added post request in a new method to save the animes to the database

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -17,21 +17,52 @@ function fetchTopAnimes() {
             animeRanking = data.data;
 
             console.log("Fetched anime ranking: ", animeRanking);
-            addAnimesToDatabase();
+            fetchAnimeInformation();
         })
         .catch(error => {
             console.error("There was an error while fetching data", error);
         })
 }
 
-function addAnimesToDatabase() {
+function fetchAnimeInformation() {
     for(anime of animeRanking) {
-        let mal_id = anime.mal_id;
-        let titleOriginal = anime.title;
-        let titleEnglish = anime.title_english;
-        let imageURL = anime.images.jpg.large_image_url;
-        let url = anime.url;
-        let releaseYear = anime.year;
+        /*
+        const mal_id = anime.mal_id;
+        const titleOriginal = anime.title;
+        const titleEnglish = anime.title_english;
+        const releaseYear = anime.year;
+        const url = anime.url;
+        const imageURL = anime.images.jpg.large_image_url;
+         */
+        const aniObject = {
+            malId : anime.mal_id,
+            titleOriginal : anime.title,
+            titleEnglish : anime.title_english,
+            releaseYear : anime.year,
+            url : anime.url,
+            imageUrl : anime.images.jpg.large_image_url
+        };
+
+        addAnimeToDatabase(aniObject);
     }
+}
+
+function addAnimeToDatabase(anime) {
+    fetch("/saveAnime", {
+        method: "POST",
+        body: JSON.stringify(anime)
+    })
+        .then(response => {
+        if(!response.ok) {
+            throw new Error("Response was not ok");
+        }
+        return response.json();
+    })
+        .then(data => {
+        console.log("Anime successfully added to database: ", data);
+    })
+        .catch(error => {
+        console.error("There was an error adding the anime to the database: ", error);
+    });
 }
 
