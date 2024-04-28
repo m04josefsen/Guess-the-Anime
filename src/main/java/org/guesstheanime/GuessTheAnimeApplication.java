d08b676088c3155661f7be2e27c373c03bc9bf96
Marco Josefsen <majos6959@oslomet.no>
2024-04-28 11:51:41 +0200

    Initial commit

---
+++ src/main/java/org/guesstheanime/GuessTheAnimeApplication.java
@@ -0,0 +1,13 @@
+package org.guesstheanime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessTheAnimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuessTheAnimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/guesstheanime/GuessTheAnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.guesstheanime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessTheAnimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
