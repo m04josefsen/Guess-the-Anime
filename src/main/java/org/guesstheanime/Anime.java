13f6d210331db75f7e10505ff99fc343619fcaea
Marco Josefsen <majos6959@oslomet.no>
2024-04-28 20:28:14 +0200

    Added normal constructer and getter and setters

--- src/main/java/org/guesstheanime/Anime.java
+++ src/main/java/org/guesstheanime/Anime.java
@@ -7,9 +7,9 @@ import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
 public class Anime {
 
     @Id
@@ -19,4 +19,63 @@ public class Anime {
     private Integer releaseYear;
     private String url;
     private String imageUrl;
+
+    public Anime(Integer malId, String titleOriginal, String titleEnglish, Integer releaseYear, String url, String imageUrl) {
+        this.malId = malId;
+        this.titleOriginal = titleOriginal;
+        this.titleEnglish = titleEnglish;
+        this.releaseYear = releaseYear;
+        this.url = url;
+        this.imageUrl = imageUrl;
+    }
+
+    public Anime(){}
+
+    public Integer getMalId() {
+        return malId;
+    }
+
+    public void setMalId(Integer malId) {
+        this.malId = malId;
+    }
+
+    public String getTitleOriginal() {
+        return titleOriginal;
+    }
+
+    public void setTitleOriginal(String titleOriginal) {
+        this.titleOriginal = titleOriginal;
+    }
+
+    public String getTitleEnglish() {
+        return titleEnglish;
+    }
+
+    public void setTitleEnglish(String titleEnglish) {
+        this.titleEnglish = titleEnglish;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Integer releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
