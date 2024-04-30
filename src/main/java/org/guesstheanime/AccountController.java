4645bde0cd4ccac92f6a05e3fea90f6278b7c543
Marco Josefsen <majos6959@oslomet.no>
2024-04-30 12:24:21 +0200

    Added logger to log successes and fails

--- src/main/java/org/guesstheanime/AccountController.java
+++ src/main/java/org/guesstheanime/AccountController.java
@@ -1,43 +1,100 @@
 package org.guesstheanime;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 public class AccountController {
 
+    private static final Logger logger = Logger.getLogger(AccountController.class.getName());
+
     @Autowired
     private AccountRepository rep;
+
+    /*
+    private String encryptPassword(String password) {
+        //TODO; encrpyt password here, in saveAccount create a new account object with the account values and the new encrypted password
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String hashedPassword = encoder.encode(password);
+        return hashedPassword;
+    }
+
+    private boolean checkPassword(String password, String hashedPassword) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        if(encoder.matches(password, hashedPassword)) {
+            return true;
+        }
+        return false;
+    }
+
+     */
 
     //Save operation
     @PostMapping("/saveAccount")
     public void saveAccount(@RequestBody Account account) {
-        rep.save(account);
+        try {
+            //Account acc = new Account(account.getEmail(), account.getFirstname(), account.getLastname(), encryptPassword(account.getPassword()), account.getHighscore());
+            rep.save(account);
+            logger.info("Account saved successfully");
+        }
+        catch (Exception e) {
+            logger.severe("Error in saveAccount: " + e.getMessage());
+        }
     }
 
     //Read operation
     @PostMapping("/listAccounts")
     public List<Account> fetchAccountList() {
-        return rep.findAll();
+        try {
+            List<Account> accountList = rep.findAll();
+            logger.info("Account updated successfully");
+            return accountList;
+        }
+        catch(Exception e) {
+            logger.severe("Error in fetchAccountList: " + e.getMessage());
+            return null;
+        }
     }
 
     @PostMapping("/updateAccount")
     public Account updateAccount(@RequestBody Account account) {
-        return rep.save(account);
+        try {
+            Account acc = rep.save(account);
+            logger.info("Account updated successfully");
+            return acc;
+        }
+        catch(Exception e) {
+            logger.severe("Error in updateAccount: " + e.getMessage());
+            return null;
+        }
     }
 
     //Delete operation
     @DeleteMapping("/deleteAccount")
-    public String deleteAccountById(@PathVariable String email) {
-        rep.deleteById(email);
-        return "Account deleted";
+    public void deleteAccountById(@PathVariable String email) {
+        try {
+            rep.deleteById(email);
+            logger.info("Account deleted successfully");
+        }
+        catch(Exception e) {
+            logger.severe("Error in deleteAccountById: " + e.getMessage());
+        }
     }
-
     @GetMapping("/getHighscore")
     public int getHighscore(@PathVariable String email) {
-        Account account = rep.getReferenceById(email);
-        return account.getHighscore();
+        try {
+            Account acc = rep.getReferenceById(email);
+            logger.info("Highscore retrieved successfully");
+            return acc.getHighscore();
+        }
+        catch(Exception e) {
+            logger.severe("Error in getHighscore: " + e.getMessage());
+            return 0;
+        }
     }
 }
--- src/main/java/org/guesstheanime/AnimeController.java
+++ src/main/java/org/guesstheanime/AnimeController.java
@@ -6,16 +6,27 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Logger;
 
 @RestController
 public class AnimeController {
+
+    private static final Logger logger = Logger.getLogger(AnimeController.class.getName());
 
     @Autowired
     AnimeRepository rep;
 
     @PostMapping("/saveAnime")
     public Anime saveAnime(@RequestBody Anime anime) {
-        return rep.save(anime);
+        try {
+            Anime ani = rep.save(anime);
+            logger.info("Anime saved successfully");
+            return ani;
+        }
+        catch(Exception e) {
+            logger.severe("Error in saveAnime: " + e.getMessage());
+            return null;
+        }
     }
 
     @GetMapping("/getRandomAnimes")
@@ -40,12 +51,26 @@ public class AnimeController {
     }
 
     public Anime getRandomAnime() {
-        List<Anime> list = rep.findAll();
-        return list.get(getRandomNumber0and25());
+        try {
+            List<Anime> list = rep.findAll();
+            logger.info("Getting random anime successfully");
+            return list.get(getRandomNumber0and25());
+        }
+        catch(Exception e) {
+            logger.severe("Error in getRandomAnime: " + e.getMessage());
+            return null;
+        }
     }
 
     public int getRandomNumber0and25() {
-        Random rand = new Random();
-        return rand.nextInt(25);
+        try {
+            Random rand = new Random();
+            logger.info("Getting random number successfully");
+            return rand.nextInt(25);
+        }
+        catch(Exception e) {
+            logger.severe("Error in getRandomNumber0and25: " + e.getMessage());
+            return 0;
+        }
     }
 }
